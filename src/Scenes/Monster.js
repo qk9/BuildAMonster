586afeeb39071c51ccf644ede17c93b442135600
qk9 <iain6475@gmail.com>
2026-04-16 18:18:36 -0700

    partially completed monster, just testing if committing works.

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -6,7 +6,17 @@ class Monster extends Phaser.Scene {
         //Create constants for the monster location
         this.bodyX = 300;
         this.bodyY = 350;
-        
+        this.mouthOffsetY = 65;
+        this.eyeLeftOffsetX = -35;
+        this.eyeLeftOffsetY = 20;
+        this.eyeRightOffsetX = 33;
+        this.eyeRightOffsetY = 10;
+        this.eyeTopOffsetX = -10;
+        this.eyeTopOffsetY = -45;
+        this.legLeftOffsetX = -55;
+        this.legLeftOffsetY = 150;
+        this.legRightOffsetX = 50;
+        this.legRightOffsetY = 150;
     }
 
     // Use preload to load art and sound assets before the scene starts running.
@@ -31,9 +41,14 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
-
-        
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenF.png");
+        my.sprite.mouth = this.add.sprite(this.bodyX, this.bodyY + this.mouthOffsetY, "monsterParts", "mouth_closed_sad.png");
+        my.sprite.eyeLeft = this.add.sprite(this.bodyX + this.eyeLeftOffsetX, this.bodyY + this.eyeLeftOffsetY, "monsterParts", "eye_red.png");
+        my.sprite.eyeRight = this.add.sprite(this.bodyX + this.eyeRightOffsetX, this.bodyY + this.eyeRightOffsetY, "monsterParts", "eye_red.png");
+        my.sprite.eyeTop = this.add.sprite(this.bodyX + this.eyeTopOffsetX, this.bodyY + this.eyeTopOffsetY, "monsterParts", "eye_psycho_dark.png");
+        my.sprite.legLeft = this.add.sprite(this.bodyX + this.legLeftOffsetX, this.bodyY + this.legLeftOffsetY, "monsterParts", "leg_darkC.png");
+        my.sprite.legLeft.flipX = true;
+        my.sprite.legRight = this.add.sprite(this.bodyX + this.legRightOffsetX, this.bodyY + this.legRightOffsetY, "monsterParts", "leg_darkC.png");
     }
 
     update() {
